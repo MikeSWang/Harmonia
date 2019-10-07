4aea669670310f28a300e7670e078714f8c060a3
MikeSbWang <mike.wang@port.ac.uk>
2019-10-07 19:03:04 +0100

    Sphinx docstring correction.

--- harmonia/cosmology/scale_dependence.py
+++ harmonia/cosmology/scale_dependence.py
@@ -1,13 +1,14 @@
-"""
+r"""
 Scale dependence (:mod:`~harmonia.cosmology.scale_dependence`)
 ===========================================================================
 
-Perform geometrical calculations.
+Compute scale-dependent modifications due to local primordial
+non-Gausianity :math:`f_\textrm{NL}`.
 
 .. autosummary::
 
-    fiducial_distance
-    redshift_to_distance
+    scale_dependent_bias
+    non_gaussianity_biased_power_spectrum
 
 |
 
@@ -19,7 +20,7 @@ _SPHERICAL_COLLAPSE_CRITICAL_OVERDENSITY = 1.686
 _SPEED_OF_LIGHT_IN_HUNDRED_KM_PER_S = 2998.
 
 
-def scale_dependent_bias(k, f_NL, b_const, cosmo):
+def scale_dependent_bias(k, f_nl, b_const, cosmo):
     """Scale-dependent bias for non-vanishing local primordial
     non-Gaussianity.
 
@@ -30,7 +31,7 @@ def scale_dependent_bias(k, f_NL, b_const, cosmo):
     ----------
     k : float, array_like
         Fourier scale/wave number.
-    f_NL : float
+    f_nl : float
         Local primordial non-Gaussnianity.
     b_const : float
         Constant linear bias.
@@ -47,20 +48,20 @@ def scale_dependent_bias(k, f_NL, b_const, cosmo):
     num_factors = 3*_SPHERICAL_COLLAPSE_CRITICAL_OVERDENSITY * cosmo.Omega0_m \
          * (cosmo.h / _SPEED_OF_LIGHT_IN_HUNDRED_KM_PER_S)**2
 
-    bias = b_const + (b_const - 1) * f_NL * num_factors \
+    bias = b_const + (b_const - 1) * f_nl * num_factors \
         / cosmology.power.transfers.CLASS(cosmo, redshift=_REDSHIFT_EPOCH)(k) \
         / k**2
 
     return bias
 
 
-def non_gaussianity_biased_power_spectrum(f_NL, b_const, cosmo):
+def non_gaussianity_biased_power_spectrum(f_nl, b_const, cosmo):
     """Biased power spectrum with non-Gaussianity scale dependence
     modification.
 
     Parameters
     ----------
-    f_NL : float
+    f_nl : float
         Local primordial non-Gaussnianity.
     b_const : float
         Constant linear bias.
@@ -76,6 +77,6 @@ def non_gaussianity_biased_power_spectrum(f_NL, b_const, cosmo):
     """
     power_spectrum = cosmology.LinearPower(cosmo, redshift=_REDSHIFT_EPOCH)
     modified_power_spectrum = lambda k: \
-        scale_dependent_bias(k, f_NL, b_const, cosmo) * power_spectrum(k)
+        scale_dependent_bias(k, f_nl, b_const, cosmo) * power_spectrum(k)
 
     return modified_power_spectrum

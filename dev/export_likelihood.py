49ae2ec68ad7aaa352a69c7c228515268f18b7d6
MikeSbWang <mike.wang@port.ac.uk>
2019-12-09 20:33:51 +0000

    Major updates.

--- dev/export_likelihood.py
+++ dev/export_likelihood.py
@@ -5,17 +5,16 @@ import os
 import sys
 from pathlib import Path
 
-_cwd = os.path.dirname(__file__)
-sys.path.insert(0, os.path.realpath(os.path.join(_cwd, "../")))
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-from view_likelihood import view_samples
+_cwd = os.path.dirname(__file__)
+sys.path.insert(0, os.path.realpath(os.path.join(_cwd, "../")))
+
+from view_likelihood import view_pdf, view_contour
 from harmonia.algorithms import DiscreteSpectrum, SphericalArray
 from harmonia.collections import (
     collate_data_files,
-    confirm_directory_path,
     harmony,
     overwrite_protection,
 )
@@ -31,15 +30,7 @@ def safe_save(data, path, file):
     path, file : str or :class:`pathlib.Path`
         Path and file name for the data to be saved.
 
-    Raises
-    ------
-    AssertionError
-        If path does not exist.
-    AssertionError
-        If overwrite permission is denied at the output path.
-
     """
-    assert confirm_directory_path(path)
     if overwrite_protection(path, file):
         try:
             np.save(path/file, data)
@@ -77,13 +68,11 @@ def filter_data(full_data, remove_degrees=()):
     )
 
     likelihood_contributions = \
-        full_data['spherical_likelihood'][:, :, ~excluded_deg]
+        full_data['spherical_likelihood'][..., ~excluded_deg]
 
     filtered_data = full_data
     filtered_data.update(
-        {
-            'spherical_likelihood': np.sum(likelihood_contributions, axis=-1)
-        }
+        {'spherical_likelihood': np.sum(likelihood_contributions, axis=-1)}
     )
 
     return filtered_data
@@ -105,27 +94,33 @@ def read_data(collate_data=False, load_data=False, save=False):
     """
     scr_dir_path = PATHOUT/script_name
     collate_path = scr_dir_path/"collated"
-    program_root = f"map={MAP},kmax={KMAX},{PRIOR},{FIXED}"
-    filename = f"{script_name}-{FILE_ROOT}-({program_root})"
+    program_root = f"map={MAP},kmax={KMAX},{PRIOR}{FIXED}"
+    file_name = f"{script_name}-{FILE_ROOT}-({program_root})"
 
     if collate_data:
-        search_root = f"map={MAP},kmax={KMAX},*{PRIOR},{FIXED}"\
+        search_root = f"map={MAP},kmax={KMAX},*{PRIOR}{FIXED}"\
             .replace("=[", "=[[]").replace("],", "[]],")
 
-        output, _, _ = collate_data_files(
+        collated_output, _, _ = collate_data_files(
             f"{scr_dir_path}/*{FILE_ROOT}-*-*{search_root}*.npy", 'npy'
         )
 
-        output['parameters'] = np.linspace(-200., 200., 401)
-        output[f'{MAP}_likelihood'] = np.squeeze(output[f'{MAP}_likelihood'])
+        # NOTE: Change this before running.
+        # output['parameter_x'] = np.linspace(-200., 200., 801)
+        # output['parameter_y'] = np.linspace(2.2, 2.6, 81)
+        collated_output['parameter'] = np.linspace(-300, 100, 401)
+
+        collated_output[f'{MAP}_likelihood'] = np.squeeze(
+            output[f'{MAP}_likelihood']
+        )
 
         if save:
-            safe_save(output, collate_path, filename + ".npy")
+            safe_save(collated_output, collate_path, file_name + ".npy")
 
     if load_data:
-        output = np.load(str(collate_path/filename) + ".npy").item()
+        collated_output = np.load(str(collate_path/file_name) + ".npy").item()
 
-    return output, filename
+    return collated_output, file_name
 
 
 def view_data(data, savefig=False, **plot_kwargs):
@@ -141,8 +136,8 @@ def view_data(data, savefig=False, **plot_kwargs):
         Keyword arguments to be passed to the plotting routine.
 
     """
-    program_root = f"map={MAP},kmax={KMAX},{PRIOR},{FIXED}"
-    filename = f"{script_name}-{FILE_ROOT}-({program_root}).pdf"
+    program_root = f"map={MAP},kmax={KMAX},{PRIOR}{FIXED}"
+    file_name = f"{script_name}-{FILE_ROOT}-({program_root}).pdf"
 
     plt.close('all')
     plt.style.use(harmony)
@@ -150,15 +145,21 @@ def view_data(data, savefig=False, **plot_kwargs):
     visual_data = data
     visual_data['likelihood'] = data[f'{MAP}_likelihood']
 
-    view_samples(
+    view_pdf(
         visual_data,
         r"$f_\mathrm{NL}$", # r"$b_1$", #
         r"$\mathcal{L}(f_\mathrm{NL})$", # r"$\mathcal{L}(b_1)$", #
         **plot_kwargs
     )
-
+    '''
+    view_contour(
+        visual_data, r"$f_\mathrm{NL}$", r"$b_1$",
+        cmap=ListedColormap(sns.color_palette('Blues')),
+        **plot_kwargs
+    )
+    '''
     if savefig:
-        plt.savefig(PATHOUT/script_name/filename)
+        plt.savefig(PATHOUT/script_name/file_name)
 
 
 if __name__ == '__main__':
@@ -168,26 +169,35 @@ if __name__ == '__main__':
 
     BOXSIZE = 1000.
 
-    MAP = "cubic"
+    MAP = "spherical"
     KHYB = 0.075
     KMAX = 0.1
     PIVOT = "spectral"
-    PRIOR = "fnl_prior=[-200.0,200.0]"
-    FIXED = "bias=2.35"
+    PRIOR = "fnl_prior=[-200.0,-200.0],"
+    FIXED = "bias=2.39"
 
     script_name = f"{MAP}_likelihood"
 
     output, filename = read_data(
-        collate_data=False,
-        load_data=True,
-        save=False
+        collate_data=True,
+        load_data=False,
+        save=True
     )
-    # filtered_output = filter_data(output, remove_degrees=())
+    filtered_output = filter_data(output, remove_degrees=())
     view_data(
         output,
         truth=0,
         precision=0,
-        norm_range=(),
+        plot_range=(),
         scatter_plot=True,
         savefig=True
     )
+    '''
+    view_data(
+        output,
+        truth=(0., None),
+        precision=(0, 2),
+        plot_ranges=[(-150., 150.), (2.2, 2.5)],
+        savefig=True
+    )
+    '''
--- dev/view_likelihood.py
+++ dev/view_likelihood.py
@@ -3,17 +3,21 @@
 """
 from __future__ import division
 
+from collections import Iterable
+
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
-from scipy.integrate import cumtrapz
+from matplotlib.colors import ListedColormap
+from scipy.integrate import cumtrapz, simps
 
 ONE_SIGMA_QUANTILES = [0.158655, 0.841345]
+SIGMA_LEVELS = [0.954500, 0.682689, 0.000001]
 
 
-def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
-                 truth=None, norm_range=(), precision=None, scatter_plot=True,
-                 xlim=None, ylim=None, fig=None):
+def view_pdf(samples, xlabel, ylabel, scaling='normalised', estimate='max',
+             truth=None, plot_range=(), precision=None, scatter_plot=True,
+             xlim=None, ylim=None, fig=None):
     """View sampled likelihood-related values.
 
     Parameters
@@ -26,12 +30,13 @@ def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
         If ``'log'``, plot the logarithmic likelihood; if ``'normalised'``
         (default), plot the likelihood normalised to full integral value of
         unity.
-    estimate : {'max', None}, optional
-        If ``'max'`` (default), the likelihood parameter estimate is marked
-        or if `None`, not marked.
+    estimate : {'max', 'median', None}, optional
+        If ``'max'`` (default), the maximum likelihood parameter estimate
+        is marked; if ``'median'``, the posterior median; or if `None`, not
+        marked.
     truth : float or None, optional
         If not `None` (default), the true parameter value is marked.
-    norm_range : float, array_like, optional
+    plot_range : float, array_like, optional
         If not an empty tuple (default), normalise likelihood in the
         parameter range specified.
     precision : int ot None, optional
@@ -50,21 +55,19 @@ def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
     """
     SCATTER_ALPHA = 1/8
 
-    if norm_range:
-        if len(norm_range) != 2:
+    if plot_range:
+        if len(plot_range) != 2:
             raise ValueError(
                 "Normalisation range must be given as exactly two end points. "
             )
-        # Firs [0] for returning the first and only axis, and the second
-        # [0] for the first and only index.
         included_range = slice(
-            np.argmin(np.abs(samples['parameters'] - norm_range[0])),
-            np.argmin(np.abs(samples['parameters'] - norm_range[1])) + 1
+            np.argmin(np.abs(samples['parameter'] - plot_range[0])),
+            np.argmin(np.abs(samples['parameter'] - plot_range[1])) + 1
         )
     else:
         included_range = slice(None, None)
 
-    parameters = samples['parameters'][included_range]
+    parameters = samples['parameter'][included_range]
     likelihoods = samples['likelihood'][:, included_range]
 
     likelihoods -= np.min(likelihoods, axis=1)[:, None]
@@ -94,8 +97,13 @@ def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
 
     main_line = plt.plot(parameters, avg_likelihood, lw=2.)
 
-    if estimate == 'max':
-        max_likelihood_parameter = parameters[np.argmax(avg_likelihood)]
+    if estimate:
+        if estimate == 'max':
+            estimate = parameters[np.argmax(avg_likelihood)]
+        elif estimate == 'median':
+            estimate = parameters[np.argmin(np.abs(
+                avg_distribution - 0.5*avg_distribution[-1]
+            ))]
         lower_bound = parameters[np.argmin(np.abs(
             avg_distribution - ONE_SIGMA_QUANTILES[0]*avg_distribution[-1]
         ))]
@@ -103,27 +111,22 @@ def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
             avg_distribution - ONE_SIGMA_QUANTILES[-1]*avg_distribution[-1]
         ))]
         if precision is not None:
-            max_likelihood_parameter = np.around(
-                max_likelihood_parameter, decimals=precision
-            )
+            estimate = np.around(estimate, decimals=precision)
             lower_uncertainty = np.around(
-                max_likelihood_parameter - lower_bound, decimals=precision
+                estimate - lower_bound, decimals=precision
             )
             upper_uncertainty = np.around(
-                upper_bound - max_likelihood_parameter, decimals=precision
+                upper_bound - estimate, decimals=precision
             )
             if precision == 0:
-                max_likelihood_parameter = int(max_likelihood_parameter)
+                estimate = int(estimate)
                 lower_uncertainty = int(lower_uncertainty)
                 upper_uncertainty = int(upper_uncertainty)
         plt.axvline(
-            x=max_likelihood_parameter,
-            ls='--',
-            c=main_line[-1].get_color(),
+            estimate,
+            ls='--', c=main_line[-1].get_color(),
             label=r"estimate ${{{}}}^{{+{}}}_{{-{}}}$".format(
-                max_likelihood_parameter,
-                upper_uncertainty,
-                lower_uncertainty
+                estimate, upper_uncertainty, lower_uncertainty
             )
         )
         plt.axvline(x=lower_bound, ls='--', c=main_line[-1].get_color())
@@ -145,5 +148,274 @@ def view_samples(samples, xlabel, ylabel, scaling='normalised', estimate='max',
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend()
+
+    return fig
+
+
+def view_contour(samples, xlabel, ylabel, truth=None, estimate=True,
+                 precision=None, plot_ranges=None, cmap=None, fig=None):
+    """View sampled likelihood contours.
+
+    Parameters
+    ----------
+    samples : dict
+        Sampled likelihood-related values.
+    xlabel, ylabel : str
+        Horizontal or vertical parameter label.
+    truth : tuple of float or None, optional
+        If not `None` (default), the true parameter values are marked.
+    estimate : bool, optional
+        If `True` (default), the parameter estimates and uncertainties are
+        marked.
+    precision : int ot None, optional
+        Number of decimal places to display in estimate labels.
+    plot_ranges : tuple of tuple of (float, float) or None, optional
+        Horizontal or vertical axis limits.
+
+    Returns
+    -------
+    fig : :class:`matplotlib.Figure`
+        Plotted figure.
+
+    """
+    xx, yy = np.meshgrid(
+        samples['parameter_x'], samples['parameter_y'], indexing='ij'
+    )
+    hh = np.exp(np.average(samples['likelihood'], axis=0)).T
+
+    mass = simps(
+        [
+            simps(likelihood_y, samples['parameter_y'])
+            for likelihood_y in hh
+        ],
+        samples['parameter_x']
+    )
+    hh /= mass
+
+    h_flat = np.flip(np.sort(hh.flatten()))
+    cum_h = np.cumsum(h_flat)
+    cum_h /= cum_h[-1]
+
+    h_levels = np.zeros_like(SIGMA_LEVELS)
+    for n, quantile in enumerate(SIGMA_LEVELS):
+        try:
+            h_levels[n] = h_flat[cum_h <= quantile][-1]
+        except IndexError:
+            h_levels[n] = h_flat[0]
+
+    if fig is None:
+        fig = plt.figure(figsize=(6, 6))
+    else:
+        plt.figure(fig.number)
+
+    if cmap is None:
+        cmap = ListedColormap(sns.color_palette('BuGn_d'))
+
+    main = plt.subplot2grid((4, 4), (1, 0), rowspan=3, colspan=3)
+
+    plt.contourf(
+        xx, yy, hh, h_levels,
+        antialiased=True, cmap=cmap
+    )
+
+    if plot_ranges:
+        try:
+            plt.xlim(*plot_ranges[0])
+        except TypeError:
+            pass
+        try:
+            plt.ylim(*plot_ranges[1])
+        except TypeError:
+            pass
+
+    if truth:
+        try:
+            plt.axvline(truth[0], c='k', ls=':', lw=1.)
+        except (TypeError, IndexError):
+            pass
+        try:
+            plt.axhline(truth[1], c='k', ls=':', lw=1.)
+        except (TypeError, IndexError):
+            pass
+
+    ax_ul = plt.subplot2grid((4, 4), (0, 0), rowspan=1, colspan=3, sharex=main)
+
+    marginal_x = np.array(
+        [simps(likelihood_y, samples['parameter_y']) for likelihood_y in hh]
+    )
+
+    plt.plot(samples['parameter_x'], marginal_x, c=cmap(cmap.N))
+
+    cumulative_x = cumtrapz(marginal_x, samples['parameter_x'], initial=0.)
+
+    if truth:
+        try:
+            plt.axvline(truth[0], c='k', ls=':', lw=1.)
+        except (TypeError, IndexError):
+            pass
+
+    if estimate:
+        median_idx = np.argmin(
+            np.abs(cumulative_x - 0.5*cumulative_x[-1])
+        )
+        median_estimate_x = samples['parameter_x'][median_idx]
+
+        lower_bound_idx = np.argmin(
+            np.abs(cumulative_x - ONE_SIGMA_QUANTILES[0]*cumulative_x[-1])
+        )
+        lower_bound_x = samples['parameter_x'][lower_bound_idx]
+
+        upper_bound_idx = np.argmin(
+            np.abs(cumulative_x - ONE_SIGMA_QUANTILES[-1]*cumulative_x[-1])
+        )
+        upper_bound_x = samples['parameter_x'][upper_bound_idx]
+
+        plt.vlines(
+            median_estimate_x,
+            ymin=0., ymax=marginal_x[median_idx],
+            colors=cmap(cmap.N), linestyles='--', linewidth=1.
+        )
+        plt.fill_between(
+            samples['parameter_x'][lower_bound_idx:upper_bound_idx+1],
+            marginal_x[lower_bound_idx:upper_bound_idx+1],
+            color=cmap(cmap.N), alpha=0.33, antialiased=True
+        )
+
+    ax_ul.spines['top'].set_visible(False)
+    ax_ul.spines['left'].set_visible(False)
+    ax_ul.spines['right'].set_visible(False)
+    plt.tick_params(
+        axis='x', which='both', direction='in',
+        top=False, labelbottom=False
+    )
+    plt.tick_params(
+        axis='y', which='both',
+        left=False, right=False, labelleft=False
+    )
+
+    if plot_ranges:
+        try:
+            plt.xlim(*plot_ranges[0])
+        except SyntaxError:
+            pass
+    plt.ylim(bottom=0, top=1.01*max(marginal_x))
+
+    ax_lr = plt.subplot2grid((4, 4), (1, 3), rowspan=3, colspan=1, sharey=main)
+
+    marginal_y = [
+        simps(likelihood_x, samples['parameter_x'])
+        for likelihood_x in hh.T
+    ]
+
+    plt.plot(marginal_y, samples['parameter_y'], c=cmap(cmap.N))
+
+    cumulative_y = cumtrapz(marginal_y, samples['parameter_y'], initial=0.)
+
+    if truth:
+        try:
+            plt.axvline(truth[1], c='k', ls=':', lw=1.)
+        except (TypeError, IndexError):
+            pass
+
+    if estimate:
+        median_idx = np.argmin(
+            np.abs(cumulative_y - 0.5*cumulative_y[-1])
+        )
+        median_estimate_y = samples['parameter_y'][median_idx]
+
+        lower_bound_idx = np.argmin(
+            np.abs(cumulative_y - ONE_SIGMA_QUANTILES[0]*cumulative_y[-1])
+        )
+        lower_bound_y = samples['parameter_y'][lower_bound_idx]
+
+        upper_bound_idx = np.argmin(
+            np.abs(cumulative_y - ONE_SIGMA_QUANTILES[-1]*cumulative_y[-1])
+        )
+        upper_bound_y = samples['parameter_y'][upper_bound_idx]
+
+        plt.hlines(
+            median_estimate_y,
+            xmin=0., xmax=marginal_y[median_idx],
+            colors=cmap(cmap.N), linestyles='--', linewidth=1.
+        )
+        plt.fill_betweenx(
+            samples['parameter_y'][lower_bound_idx:upper_bound_idx+1],
+            marginal_y[lower_bound_idx:upper_bound_idx+1],
+            edgecolor='none',
+            color=cmap(cmap.N),
+            alpha=0.33,
+            antialiased=True
+        )
+
+    ax_lr.spines['top'].set_visible(False)
+    ax_lr.spines['bottom'].set_visible(False)
+    ax_lr.spines['right'].set_visible(False)
+    plt.tick_params(
+        axis='x', which='both',
+        bottom=False, top=False, labelbottom=False
+    )
+    plt.tick_params(
+        axis='y', which='both', direction='in',
+        right=False, labelleft=False
+    )
+
+    if plot_ranges:
+        try:
+            plt.ylim(*plot_ranges[1])
+        except SyntaxError:
+            pass
+    plt.xlim(left=0, right=1.01*max(marginal_y))
+
+    if estimate:
+        max_indices = np.unravel_index(np.argmax(hh), hh.shape)
+        main.scatter(
+            xx[max_indices], yy[max_indices],
+            s=64, c=cmap(cmap.N), marker='+', label='maximum'
+        )
+        main.scatter(
+            median_estimate_x, median_estimate_y,
+            s=64, c=cmap(cmap.N), marker='x', label='median'
+        )
+
+    main.legend(handletextpad=0)
+
+    if estimate:
+        if isinstance(precision, Iterable):
+            median_estimate_x = np.around(median_estimate_x, decimals=precision[0])
+            lower_uncertainty_x = np.around(
+                median_estimate_x - lower_bound_x, decimals=precision[0]
+            )
+            upper_uncertainty_x = np.around(
+                upper_bound_x - median_estimate_x, decimals=precision[0]
+            )
+            if precision[0] == 0:
+                median_estimate_x = int(median_estimate_x)
+                lower_uncertainty_x = int(lower_uncertainty_x)
+                upper_uncertainty_x = int(upper_uncertainty_x)
+
+            median_estimate_y = np.around(median_estimate_y, decimals=precision[1])
+            lower_uncertainty_y = np.around(
+                median_estimate_y - lower_bound_y, decimals=precision[1]
+            )
+            upper_uncertainty_y = np.around(
+                upper_bound_y - median_estimate_y, decimals=precision[1]
+            )
+            if precision[1] == 0:
+                median_estimate_y = int(median_estimate_y)
+                lower_uncertainty_y = int(lower_uncertainty_y)
+                upper_uncertainty_y = int(upper_uncertainty_y)
+        main.set_xlabel(
+            r"{} = ${{{}}}^{{+{}}}_{{-{}}}$".format(
+                xlabel, median_estimate_x, upper_uncertainty_x, lower_uncertainty_x
+            )
+        )
+        main.set_ylabel(
+            r"{} = ${{{}}}^{{+{}}}_{{-{}}}$".format(
+                ylabel, median_estimate_y, upper_uncertainty_y, lower_uncertainty_y
+            )
+        )
+    else:
+        main.set_xlabel(xlabel)
+        main.set_ylabel(ylabel)
 
     return fig

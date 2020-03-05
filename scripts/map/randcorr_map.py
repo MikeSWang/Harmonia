1bb9f7d3f02c87a210b65abfc898e784077ba512
MikeSbWang <mike.wang@port.ac.uk>
2020-03-05 19:09:53 +0000

    Script in progress.

--- scripts/map/randcorr_map.py
+++ scripts/map/randcorr_map.py
@@ -1,151 +1,124 @@
-"""Generate correlated random maps.
+"""Generate random correlated hybrid maps.
 
 """
-from itertools import chain
+from argparse import ArgumentParser
+from collections import defaultdict
+from pprint import pprint
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from nbodykit.lab import ConvolvedFFTPower, FKPCatalog, UniformCatalog
 
-from map_rc import PATHOUT
+from map_rc import PATHOUT, script_name, domain_cut
+from harmonia.algorithms import DiscreteSpectrum
 from harmonia.algorithms import CartesianArray, SphericalArray
-from harmonia.collections import harmony
-from harmonia.collections import \
-    covar_to_corr, overwrite_protection, sort_dict_to_list
-
-CATALOGUE_SERIAL_NUM = list(chain(range(0, 11), range(12, 22)))
-CATALOGUE_SERIAL_IND = ["L.txt", "R.txt"]
-
-
-def safe_save(data, path, file):
-    """Safely save data by checking overwrite protections.
-
-    Parameters
-    ----------
-    data : array_like
-        Data to be saved.
-    path, file : str or :class:`pathlib.Path`
-        Path and file name for the data to be saved.
-
-    """
-    if overwrite_protection(path, file):
-        try:
-            np.save(path/file, data)
-        except TypeError:
-            np.save(path + file, data)
+from harmonia.mapper import CartesianMap, SphericalMap
+from harmonia.collections import (
+    collate_data_files,
+    confirm_directory_path,
+    covar_to_corr,
+    harmony,
+    overwrite_protection,
+    sort_dict_to_list,
+)
 
 
-def get_hybrid_maps():
-    """Load and process hybrid map data files.
+def parse_args():
+    """Parse command-line argument inputs.
 
     Returns
     -------
-    hybrid_map_data : list of :class:`numpy.ndarray`
-        Hybrid map data vectors.
+    :class:`argparse.Namespace`
+        Parsed parameter space.
 
     """
-    output_filename = (
-        "halos-(NG={},z=1.)-(map=hybrid,fsky={},"
-        "pivots=[{},{}],knots=[{},{}],orders={},rsd={}).npy"
-    ).format(NG, FSKY, SPIVOT, CPIVOT, KHYB, KMAX, ORDERS, RSD)
+    parser = ArgumentParser()
 
-    catalogue_root = "halos-(NG={},z=1.)-{}-"
-    smap_suffix = "(map=spherical,fsky={},knots=[{},{}],rsd={}).npy"
-    cmap_suffix = "(map=cartesian,fsky={},knots=[{},{}],orders={},rsd={}).npy"
+    parser.add_argument('--task', choices=['gen', 'agg'])
+    parser.add_argument('--batchno', default='')
 
-    if COLLATE_DATA:
-        hybrid_map_data = []
-        for serial_num in CATALOGUE_SERIAL_NUM:
-            catalogue_prefix = catalogue_root.format(NG, serial_num)
+    parser.add_argument('--fsky', type=float, default=1.)
 
-            smap_file = catalogue_prefix \
-                + smap_suffix.format(FSKY, None, KHYB, RSD)
-            cmap_file = catalogue_prefix \
-                + cmap_suffix.format(FSKY, KHYB, KMAX, ORDERS, RSD)
+    parser.add_argument('--pivot', default='spectral')
 
-            smap_data = np.load(PATHOUT/"spherical_map"/smap_file).item()
-            cmap_data = np.load(PATHOUT/"cartesian_map"/cmap_file).item()
+    parser.add_argument('--kmin', type=float, default=None)
+    parser.add_argument('--khyb', type=float, default=0.04)
+    parser.add_argument('--kmax', type=float, default=0.1)
 
-            for serial_index in CATALOGUE_SERIAL_IND:
-                smap_vector = SphericalArray.build(
-                    filling=sort_dict_to_list(smap_data[serial_index])
-                ).unfold(SPIVOT, return_only='data')
+    parser.add_argument('--orders', type=int, nargs='+', default=[0])
 
-                cmap_vector = CartesianArray(
-                    cmap_data[serial_index], 'k', 'power_'
-                ).unfold(CPIVOT, return_only='data')
+    parser.add_argument('--nbar', type=float, default=2.5e-4)
+    parser.add_argument('--contrast', type=float, default=10.)
+    parser.add_argument('--boxsize', type=float, default=1000.)
+    parser.add_argument('--dk', type=float, default=None)
+    parser.add_argument('--mesh', type=int, default=256)
+    parser.add_argument('--iter', type=int, default=100)
 
-                hybrid_map_data.append(
-                    np.concatenate((smap_vector, cmap_vector))
-                )
-        safe_save(hybrid_map_data, PATHOUT/"hybrid_map", output_filename)
-    else:
-        hybrid_map_data = np.load(PATHOUT/"hybrid_map"/output_filename)
-
-    return hybrid_map_data
+    return parser.parse_args()
 
 
-def inspect_hybrid_map(thredshold=0., savefig=False, zoom=False):
-    """Inspect statistical properties of hybrid map data.
+def initialise():
+    """Initialise program with parsed parameters.
 
     Returns
     -------
-    sample_corr : :class:`numpy.ndarray`
-        Hybrid map data sample correlation matrix.
+    str
+        Initialisation tag.
 
     """
-    mean_data = np.average(hybrid_data, axis=0)
-    sample_cov = np.conj(hybrid_data - mean_data).T \
-        @ np.asarray(hybrid_data - mean_data) \
-        / len(hybrid_data)
-    sample_corr = covar_to_corr(sample_cov)
+    param_dict = vars(params)
 
-    reflection_mask = np.triu_indices(len(sample_cov), k=1)
-    view_corr = sample_corr.real
-    view_corr[reflection_mask] = sample_corr.imag[reflection_mask]
+    pprint(param_dict)
 
-    output_filename = "hybrid_corr-(fsky={},pivots=[{},{}],rsd={}).pdf"\
-        .format(FSKY, SPIVOT, CPIVOT, RSD)
-
-    plt.close('all')
-    plt.style.use(harmony)
-    sns.set(style='ticks', font='serif')
-
-    view_offcorr = view_corr[zoom:, :zoom]  # real part only
-
-    view_corr[np.abs(view_corr) < thredshold] = 0.
-    sns.heatmap(
-        (view_corr), square=True,
-        cmap='coolwarm', rasterized=True
+    return (
+        "fsky={:.2f},knots=[{},{},{}],orders={},boxsize={:.0f},iter={:d}"
+    ).format(
+        params.fsky, params.kmin, params.khyb, params.kmax,
+        str(params.orders).replace(", ", ","), params.boxsize, params.iter
     )
-    if savefig:
-        plt.savefig(
-            PATHOUT/"hybrid_map"/output_filename.replace("_corr", "_corr"),
-            format='pdf', transparency=True
+
+
+def generate():
+    """Generate FKP-paired random catalogues with the desired survey window
+    and process them into power spectrum measurements.
+
+    Returns
+    -------
+    results : dict
+        Multipole measurements.
+
+    """
+    results = {'cmap': defaultdict(list), 'smap': defaultdict(list)}
+    for _ in range(params.iter):
+        data_catalogue = UniformCatalog(params.nbar, params.boxsize)
+        rand_catalogue = UniformCatalog(
+            params.contrast*params.nbar, params.boxsize
         )
-    if zoom:
-        plt.figure()
-        sns.heatmap((view_offcorr), cmap='coolwarm')
-        if savefig:
-            plt.savefig(
-                PATHOUT/"hybrid_map"/(
-                    output_filename.replace("_corr", "_offcorr")
-                ),
-                format='pdf', transparency=True
+
+        for catalogue in [data_catalogue, rand_catalogue]:
+            catalogue['Selection'] *= domain_cut(
+                catalogue['Position'] - [params.boxsize/2] * 3,
+                params.boxsize/2, params.fsky,
             )
+            catalogue['NZ'] = params.nbar * catalogue['Weight']
 
-    return sample_corr
+        catalogue_pair = FKPCatalog(data_catalogue, rand_catalogue)
+        cartesian_map = CartesianMap(catalogue_pair, num_mesh=params.mesh)
+        cartesian_power = cartesian_map.power_multipoles(
+            params.orders, kmin=params.khyb, kmax=params.kmax, dk=params.dk
+        )
 
+        results['k'].append(cartesian_power['k'])
+        results['Nk'].append(cartesian_power['Nk'])
+        for ell in params.orders:
+            results[f'power_{ell}'].append(cartesian_power[f'power_{ell}'])
 
-COLLATE_DATA = True
+    return results
 
-NG = "0."
-FSKY = "1.00"
-KHYB, KMAX = 0.04, 0.1
-ORDERS = "[0]"
-SPIVOT, CPIVOT = 'natural', 'variable'
-RSD = False
 
 if __name__ == '__main__':
-    hybrid_data = get_hybrid_maps()
-    hybrid_corr = inspect_hybrid_map(thredshold=0., savefig=True, zoom=456)
+
+    params = parse_args()
+    tag = initialise()
+

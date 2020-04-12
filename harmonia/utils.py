ee1cb32777d5d95937590e0b91aab3d120560f50
Mike Shengbo Wang <mike.wang@port.ac.uk>
2020-04-12 16:52:16 +0100

    Syntax updated.

--- harmonia/utils.py
+++ harmonia/utils.py
@@ -603,7 +603,7 @@ def covar_to_corr(covar):
         Correlation matrix.
 
     """
-    inv_diag = np.diag(np.power(np.abs(np.diag(covar)), -1/2))
+    inv_diag = np.diag(np.power(np.abs(np.diag(covar)), - 1./2.))
     corr = inv_diag @ covar @ inv_diag
 
     return corr
